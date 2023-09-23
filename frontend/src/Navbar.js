175e316c8dc75a8b528f5a496d3fcaae0fc90ea8
JoeSirio <joe.sirio22@gmail.com>
2023-09-23 11:26:39 -0700

    remove unused navbar items

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -3,8 +3,8 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>M.O.</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/Calendar">Calendar</a>
+                {/* <a href="/">Home</a>
+                <a href="/Calendar">Calendar</a> */}
             </div>
         </nav>
      );
